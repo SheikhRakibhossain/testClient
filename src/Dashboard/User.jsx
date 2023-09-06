85931f5940718c304426be3a65f048cbcb65408c
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-09-07 00:31:39 +0600

    data deleted function added with relaod

--- src/Dashboard/User.jsx
+++ src/Dashboard/User.jsx
@@ -1,13 +1,33 @@
 import { useLoaderData } from "react-router-dom";
 import { Button, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from "@nextui-org/react";
+import Swal from "sweetalert2";
 
 const User = () => {
 
     const users = useLoaderData();
 
     const handleDelete = _id =>{
-        
         console.log(_id)
+        fetch(`http://localhost:5000/user/${_id}`,{
+
+        method:'DELETE'
+        })
+        .then(res =>res.json())
+        .then(data => {
+            console.log(data);
+            if(data.deletedCount > 0){
+                return( Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: 'The user has been deleted from our database.',
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
+                  )
+                }
+            })
+            window.location.reload()
+
     }
 
     return (
@@ -21,8 +41,8 @@ const User = () => {
               <TableColumn>Delete</TableColumn>
             </TableHeader>
             <TableBody>
-              {users?.map(user => <TableRow key={user.id}
-              >
+              {users.map(user => <TableRow key={user._id}>
+              
                 <TableCell>{user.id}</TableCell>
                 <TableCell>{user.name}</TableCell>
                 <TableCell>{user.status}</TableCell>
