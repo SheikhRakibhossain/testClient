ad4989fe1a202d111fbc9342333b05edc1ddeb3e
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-09-06 14:48:55 +0600

    user data have collection issues fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Button, Input, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from "@nextui-org/react";
 import { useState } from "react";
+import Swal from 'sweetalert2'
+
 // import { Form } from "react-router-dom";
 
 const App = () => {
@@ -28,7 +30,17 @@ const App = () => {
     })
       .then(res => res.json())
       .then(data => {
-        console.log(data)
+        console.log(data);
+        if(data.insertedId){
+          return Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'The user has been added into our database.',
+            showConfirmButton: false,
+            timer: 1500
+          })
+          
+        }
 
       })
       .catch(error => console.log(error.message))
@@ -43,10 +55,10 @@ const App = () => {
         <div className=" flex flex-wrap justify-center items-center md:flex-nowrap gap-4 py-4">
           <form onSubmit={handleForm}>
             <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
-              <Input variant="underlined" type="text" label="Name" name='name' />
-              <Input variant="underlined" type="text" label="Status" name='status' maxlength="10" />
-              <Input variant="underlined" type="email" label="Email" name='email' />
-              <Input variant="underlined" type="password" label="Password" name='password' />
+              <Input isRequired variant="underlined" type="text" label="Name" name='name'  />
+              <Input variant="underlined" type="text" label="Status" name='status' maxlength="10" isRequired />
+              <Input variant="underlined" type="email" label="Email" name='email' isRequired/>
+              <Input variant="underlined" type="password" label="Password" name='password' isRequired />
               <Button type="submit" color="primary" variant="shadow">Submit</Button>
             </div>
           </form>
